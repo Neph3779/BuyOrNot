b5e39a1e6c1b1af7f9243c99256c4b320a62195e
Neph <tony3322@naver.com>
2021-11-16 00:18:53 +0900

    [Docs] 1차 기획안(스토리보드) 업로드

--- BuyOrNotDemo/AppDelegate.swift
+++ BuyOrNotDemo/AppDelegate.swift
@@ -14,6 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        UITabBar.appearance().barTintColor = .black
+        UITabBar.appearance().tintColor = .systemIndigo
         return true
     }
 
