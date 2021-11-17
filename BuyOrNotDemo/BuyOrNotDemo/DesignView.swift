87b6bd6263f2ef9900628edb9d9e5e974ab968b0
Neph <tony3322@naver.com>
2021-11-18 00:24:16 +0900

    [Chore] Demo에 기획 세부 사항 디테일 추가, 리드미 업데이트

--- BuyOrNotDemo/BuyOrNotDemo/DesignView.swift
+++ BuyOrNotDemo/BuyOrNotDemo/DesignView.swift
@@ -2,6 +2,41 @@ import UIKit
 
 @IBDesignable
 class DesignableView: UIView {
+    @IBInspectable var firstColor: UIColor = UIColor.clear {
+        didSet {
+            updateView()
+        }
+    }
+
+    @IBInspectable var secondColor: UIColor = UIColor.clear {
+        didSet {
+            updateView()
+        }
+    }
+
+    @IBInspectable var isHorizontal: Bool = true {
+        didSet {
+            updateView()
+        }
+    }
+
+    override class var layerClass: AnyClass {
+        get {
+            return CAGradientLayer.self
+        }
+    }
+
+    func updateView() {
+        let layer = self.layer as! CAGradientLayer
+        layer.colors = [firstColor, secondColor].map {$0.cgColor}
+        if (isHorizontal) {
+            layer.startPoint = CGPoint(x: 0, y: 0.5)
+            layer.endPoint = CGPoint (x: 1, y: 0.5)
+        } else {
+            layer.startPoint = CGPoint(x: 0.5, y: 0)
+            layer.endPoint = CGPoint (x: 0.5, y: 1)
+        }
+    }
 }
 
 @IBDesignable
@@ -102,4 +137,5 @@ extension UIView {
             }
         }
     }
+
 }
