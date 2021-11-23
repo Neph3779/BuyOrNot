18a5d6c732954d0ef564690a5185c24f0ef3a490
Neph <tony3322@naver.com>
2021-11-23 21:21:07 +0900

    [Feat] Raking 관련 데이터를 담을 모델과 RankManager 제작

---
+++ BuyOrNot/BuyOrNot/RankedProduct.swift
@@ -0,0 +1,15 @@
+//
+//  ProductRank.swift
+//  BuyOrNot
+//
+//  Created by 천수현 on 2021/11/22.
+//
+
+import Foundation
+
+struct RankedProduct {
+    let brand: String
+    let name: String
+    var rank: Int
+    let image: URL
+}
