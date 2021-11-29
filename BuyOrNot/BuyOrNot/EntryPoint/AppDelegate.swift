75c3c3280304512431b270609f2bcc66935db8a8
Neph <tony3322@naver.com>
2021-11-30 03:01:19 +0900

    [Fix] 세로 모드 고정 project 파일에서 해결

--- BuyOrNot/BuyOrNot/EntryPoint/AppDelegate.swift
+++ BuyOrNot/BuyOrNot/EntryPoint/AppDelegate.swift
@@ -23,8 +23,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.makeKeyAndVisible()
         return true
     }
-
-//    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
-//            return UIInterfaceOrientationMask.portrait
-//    }
 }
