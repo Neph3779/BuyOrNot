1dd90007a1e79a5b43f62522f2b2dccfe28abe48
Neph <tony3322@naver.com>
2021-12-02 10:57:58 +0900

    [Fix] 잘못된 파일에 복사했던 것 수정

--- BuyOrNot/BuyOrNot/Controller/ReviewFetcher.swift
+++ BuyOrNot/BuyOrNot/Controller/ReviewFetcher.swift
@@ -1,57 +1,166 @@
 //
-//  YoutubeCrawler.swift
+//  ReviewFetcher.swift
 //  BuyOrNot
 //
-//  Created by 천수현 on 2021/12/01.
+//  Created by 천수현 on 2021/12/02.
 //
 
 import Foundation
-import SwiftSoup
 import Alamofire
 
-final class YoutubeCrawler {
-    private let baseURL = "https://www.youtube.com/results?search_query="
-    private init() {}
-    static let shared = YoutubeCrawler()
+protocol ReviewFetcherDelegate: AnyObject {
+    func setJoinedReview(reviewContents: [ReviewContent])
+    func reloadData()
+}
 
-    func fetchYoutubeReviews(query: String, completion: @escaping ([YoutubeCrawlingResult.VideoContent]?) -> Void) {
-        let searchQuery = query.split(separator: " ").joined(separator: "+")
+final class ReviewFetcher {
+    weak var reviewFetcherDelegate: ReviewFetcherDelegate?
+    private var product: Product
+    private var youtubeResults = [YoutubeVideosResult]()
+    private var naverResults: NaverBlogResult?
+    private var tistoryResults: KakaoBlogResult?
+    private var joinedReview = [ReviewContent]()
 
-        guard let encodedString = (baseURL + searchQuery)
-                .addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
-              let url = URL(string: encodedString) else { return }
+    private var didYoutubeFetchingDone = false
+    private var didNaverFetchingDone = false
+    private var didTistoryFetchingDone = false
 
-        AF.request(url, method: .get).responseString { response in
-            do {
-                let htmlSource = try response.result.get()
-                let script = try SwiftSoup.parse(htmlSource).select("body[dir]").select("script[nonce]").array()
-                let filtered = try script.filter { element in
-                    try element.html().contains("ytInitialData") // var ytInitialData = JSON FILE; 형태로 저장되어있음
+    private var didFetchingDone: Bool {
+        return didYoutubeFetchingDone && didNaverFetchingDone && didTistoryFetchingDone
+    }
+
+    init(product: Product, delegate: ReviewFetcherDelegate) {
+        self.product = product
+        self.reviewFetcherDelegate = delegate
+    }
+
+    func fetchReviews() {
+        fetchYoutubeVideoIds()
+        fetchNaverBlogReviews()
+        fetchTistoryBlogReviews()
+    }
+
+    private func fetchYoutubeVideoIds() {
+        YoutubeCrawler.shared.fetchYoutubeReviews(query: "\(product.brand) \(product.name) 리뷰",
+                                                  completion: youtubeVideoIdFetchCompletion(contents:))
+
+    }
+
+    private func youtubeVideoIdFetchCompletion(contents: [YoutubeCrawlingResult.VideoContent]?) {
+        var videoIds = [String?]()
+        guard let contents = contents else { return }
+        contents.forEach { videoIds.append($0.videoRenderer?.videoId) }
+        let videoIdsWithoutNil = videoIds.compactMap { $0 }
+
+        for index in 0 ..< videoIdsWithoutNil.count {
+            YoutubeAPIClient.shared.fetchYoutubeVideoById(videoId: videoIdsWithoutNil[index]) { [weak self]
+                (response: DataResponse<YoutubeVideosResult, AFError>) in
+                guard let self = self else { return }
+                do {
+                    let result = try JSONDecoder().decode(YoutubeVideosResult.self, from: response.data!)
+                    self.youtubeResults.append(result)
+                } catch {
+
                 }
-
-                if let jsonData = filtered.first {
-                    let dataString = String(try jsonData.html()
-                                                .replacingOccurrences(of: "var ytInitialData = ", with: "")
-                                                .dropLast()) // var 선언 부분과 마지막 문자 ;를 제거하여 JSON 파일 형태로 만들어줌
-                    let data = Data(dataString.utf8)
-                    let result = try JSONDecoder().decode(YoutubeCrawlingResult.self, from: data)
-                    var contentIndex = 0
-
-                    while result.contents?.twoColumnSearchResultsRenderer?
-                            .primaryContents?.sectionListRenderer?.contents?[contentIndex].itemSectionRenderer?.contents?[0]
-                            .videoRenderer == nil {
-                        contentIndex += 1
+                if index == videoIdsWithoutNil.count - 1 {
+                    self.didYoutubeFetchingDone = true
+                    if self.didFetchingDone {
+                        self.joinReviews()
+                        self.reviewFetcherDelegate?.reloadData()
                     }
-                    
-                    let contents = result.contents?.twoColumnSearchResultsRenderer?
-                        .primaryContents?.sectionListRenderer?
-                        .contents?[contentIndex].itemSectionRenderer?.contents
-
-                    completion(contents)
                 }
-            } catch {
-
             }
         }
+    }
+
+    private func fetchNaverBlogReviews() {
+        NaverSearchAPIClient.shared
+            .fetchNaverBlogResults(query: "\(product.brand) \(product.name) 리뷰", count: 20) { [weak self]
+                (response: DataResponse<NaverBlogResult, AFError>) in
+                guard let self = self else { return }
+                do {
+                    self.naverResults = try JSONDecoder().decode(NaverBlogResult.self, from: response.data!)
+                } catch {
+
+                }
+                self.didNaverFetchingDone = true
+                if self.didFetchingDone {
+                    self.joinReviews()
+                    self.reviewFetcherDelegate?.reloadData()
+                }
+            }
+    }
+
+    private func fetchTistoryBlogReviews() {
+        KakaoAPIClient.shared
+            .fetchKakaoBlogPosts(query: "\(product.brand) \(product.name) 리뷰", count: 20) { [weak self]
+                (response: DataResponse<KakaoBlogResult, AFError>) in
+                guard let self = self else {
+                    return
+                }
+                do {
+                    self.tistoryResults = try JSONDecoder().decode(KakaoBlogResult.self, from: response.data!)
+                } catch {
+
+                }
+                self.didTistoryFetchingDone = true
+                if self.didFetchingDone {
+                    self.joinReviews()
+                    self.reviewFetcherDelegate?.reloadData()
+                }
+            }
+    }
+
+    private func joinReviews() {
+        let youtubeReviews: [ReviewContent]? = youtubeResults.map { result in
+            let item = result.items[0]
+            let thumbnailURL = URL(string: item.snippet.thumbnails.high.url)
+            let link = URL(string: "https://www.youtube.com/watch?v=" + item.id)
+
+            return ReviewContent(siteKind: .youtube, title: item.snippet.title,
+                                 producerName: item.snippet.channelTitle,
+                                 thumbnail: thumbnailURL, link: link, youtubeId: item.id)
+        }
+
+        let tistoryReviews: [ReviewContent]? = tistoryResults?.documents.map { item -> ReviewContent in
+            let siteKind: ReviewSiteKind = item.link.contains("naver") ? .naver : .tistory
+            let thumbnailURL = URL(string: item.thumbnail)
+            let link = URL(string: item.link)
+            return ReviewContent(siteKind: siteKind, title: item.title,
+                                 producerName: item.blogName,
+                                 thumbnail: thumbnailURL, link: link, youtubeId: nil)
+        }
+
+        let naverReviews: [ReviewContent]? = naverResults?.items.map { item -> ReviewContent? in
+            let link = URL(string: item.link)
+            if let tistoryReviews = tistoryReviews {
+                if tistoryReviews.filter({ $0.producerName == item.bloggerName }).isEmpty {
+                    return ReviewContent(siteKind: .naver, title: item.title,
+                                         producerName: item.bloggerName,
+                                         thumbnail: nil, link: link, youtubeId: nil)
+                }
+            }
+            return nil
+        }.compactMap { $0 }
+
+        var count = 0
+
+        while count < youtubeReviews?.count ?? 0 || count < naverReviews?.count ?? 0 || count < tistoryReviews?.count ?? 0 {
+            if count < youtubeReviews?.count ?? 0 {
+                joinedReview.append(youtubeReviews![count])
+            }
+
+            if count < tistoryReviews?.count ?? 0 {
+                joinedReview.append(tistoryReviews![count])
+            }
+
+            if count < naverReviews?.count ?? 0 {
+                joinedReview.append(naverReviews![count])
+            }
+
+            count += 1
+        }
+
+        reviewFetcherDelegate?.setJoinedReview(reviewContents: joinedReview)
     }
 }
